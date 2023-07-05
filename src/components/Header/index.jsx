05a5f845fc6d9407e9069893133d10c379c4627f
Mathis Méheux <mat.mhx@laposte.net>
2023-07-06 01:43:37 +0200

    Header stylisé.

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,14 +1,37 @@
-// import Logo from "../../assets/LOGO.png"
+import Logo from "../../assets/LOGO.png"
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import colors from "../../utils/style/colors"
+
+
+const HeaderBalise = styled.header`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 25px 55px;
+    border: 1px solid black;
+`
+const HeaderLogo = styled.img`
+    width: 135px;
+`
+const StyledLink = styled(Link)`
+    margin: 20px;
+    text-decoration: none;
+    color: ${colors.primary};
+    &:hover {
+        text-decoration: underline;
+    }
+`
 
 function Header() {
     return (
-        <header>
+        <HeaderBalise>
+            <HeaderLogo src={Logo} alt="logo" />
             <nav>
-                <Link to="/">Accueil</Link>
-                <Link to="/about">About</Link>
+                <StyledLink to="/">Accueil</StyledLink>
+                <StyledLink to="/about">A Propos</StyledLink>
             </nav>
-        </header>
+        </HeaderBalise>
     )
 }
 
