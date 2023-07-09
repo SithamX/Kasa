add80699f4c12a1d2fa25c382fb561f6f2c97166
Mathis Méheux <mat.mhx@laposte.net>
2023-07-10 01:33:33 +0200

    Suppression vieu code que j'avais mis entre commentaires.

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -2,41 +2,6 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import colors from '../../utils/style/colors';
 
-/* const CardSection = styled.section`
-    background-color: ${colors.secondary};
-`
-const CardHousing = styled.div`
-    background: linear-gradient(to bottom, ${colors.primary}, ${colors.black} 99.999%);
-    height: 260px;
-    width: 250px;
-    border-radius: 10px;
-`
-const HeaderLogo = styled.img`
-    width: 135px;
-`
-const StyledLink = styled(Link)`
-    margin-left: 40px;
-    text-decoration: none;
-    color: ${colors.primary};
-    font-weight: 300;
-    &:hover {
-        text-decoration: underline;
-    }
-`
-
-function Card() {
-    return (
-        <CardSection>
-            <CardHousing />
-            <HeaderLogo src={Logo} alt="logo" />
-                <StyledLink to="/">Accueil</StyledLink>
-                <StyledLink to="/about">A Propos</StyledLink>
-        </CardSection>
-    )
-}
-
-export default Card */
-
 const CardLink = styled(Link)`
     position: relative;
     display: inline-block;
